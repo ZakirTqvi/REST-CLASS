f483b86c1bc9a1bfc55dbb61d712db12c3418b24
ZakirTqvi <husainitaqvi4@gmail.com>
2025-02-23 23:25:37 +0530

    code is added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+
 
 const port = 3000;
 
@@ -13,14 +15,16 @@ app.set("views", path.join(__dirname, "/views"));
 
 let posts = [
     {
+       id: uuidv4(),
        username:"Zakir",
        content: "I am feeling good"
     },
-    {
+    {  id: uuidv4(),
        username:"Shadab",
        content: "I am learning web dev"
     },
     {
+       id: uuidv4(),
        username:"Zafar",
        content: "Hi!..."
     }
@@ -36,8 +40,25 @@ app.get("/posts/new", (req, res) => {
 
 app.post("/posts", (req, res) => {
     let { username, content } = req.body;
-    posts.push({username, content});
+    let id = uuidv4();
+    posts.push({ id, username, content});
     res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    console.log(id);
+    let post = posts.find((p)=> id === p.id);
+    post.content = newContent;
+    console.log(post);
+    res.send("patch request working");
 });
 
 app.listen(port, () => {
