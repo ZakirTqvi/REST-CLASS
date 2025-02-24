0d7179d02cf7b7dcdccbbef25126441a5699fbce
ZakirTqvi <husainitaqvi4@gmail.com>
2025-02-24 11:35:27 +0530

    code is added

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 
 const port = 3000;
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -58,8 +62,20 @@ app.patch("/posts/:id", (req, res) => {
     let post = posts.find((p)=> id === p.id);
     post.content = newContent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
+
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p)=> id === p.id);
+    res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
 
 app.listen(port, () => {
     console.log(`Server is listening at port ${port}`)
